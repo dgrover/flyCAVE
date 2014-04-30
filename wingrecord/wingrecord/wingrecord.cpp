96d527d103d49f4a8ff7d43849b95cfc8254c035
Dhruv Grover <dhgrover@ucsd.edu>
2014-04-29 17:11:41 -0700

    Split recording and tracking in to two projects

---
+++ wingrecord/wingrecord/wingrecord.cpp
@@ -0,0 +1,372 @@
+// wingtrack.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "FlyCapture2.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace FlyCapture2;
+using namespace cv;
+
+Camera cam;
+FILE *FMF_Out;
+FILE *flog;
+
+unsigned __int32 fmfVersion, SizeY, SizeX;
+unsigned __int64 bytesPerChunk, nframes;
+char *buf;
+
+double stamp;
+FlyCapture2::TimeStamp timestamp;
+
+Mat frame;
+
+char fname[100];
+char flogname[100];
+
+void PrintBuildInfo()
+{
+	FC2Version fc2Version;
+	Utilities::GetLibraryVersion(&fc2Version);
+	char version[128];
+	sprintf_s(
+		version,
+		"FlyCapture2 library version: %d.%d.%d.%d\n",
+		fc2Version.major, fc2Version.minor, fc2Version.type, fc2Version.build);
+
+	printf("%s", version);
+
+	char timeStamp[512];
+	sprintf_s(timeStamp, "Application build date: %s %s\n\n", __DATE__, __TIME__);
+
+	printf("%s", timeStamp);
+}
+
+void PrintCameraInfo(CameraInfo* pCamInfo)
+{
+	printf(
+		"\n*** CAMERA INFORMATION ***\n"
+		"Serial number - %u\n"
+		"Camera model - %s\n"
+		"Camera vendor - %s\n"
+		"Sensor - %s\n"
+		"Resolution - %s\n"
+		"Firmware version - %s\n"
+		"Firmware build time - %s\n\n",
+		pCamInfo->serialNumber,
+		pCamInfo->modelName,
+		pCamInfo->vendorName,
+		pCamInfo->sensorInfo,
+		pCamInfo->sensorResolution,
+		pCamInfo->firmwareVersion,
+		pCamInfo->firmwareBuildTime);
+}
+
+void PrintFormat7Capabilities(Format7Info fmt7Info)
+{
+	printf(
+		"Max image pixels: (%u, %u)\n"
+		"Image Unit size: (%u, %u)\n"
+		"Offset Unit size: (%u, %u)\n"
+		"Pixel format bitfield: 0x%08x\n",
+		fmt7Info.maxWidth,
+		fmt7Info.maxHeight,
+		fmt7Info.imageHStepSize,
+		fmt7Info.imageVStepSize,
+		fmt7Info.offsetHStepSize,
+		fmt7Info.offsetVStepSize,
+		fmt7Info.pixelFormatBitField);
+}
+
+void PrintError(Error error)
+{
+	error.PrintErrorTrace();
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	bool record = false;
+
+	FMF_Out = new FILE;
+	flog = new FILE;
+
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+
+	Error error;
+
+	const Mode k_fmt7Mode = MODE_0;
+	const PixelFormat k_fmt7PixFmt = PIXEL_FORMAT_RAW8;
+
+	BusManager busMgr;
+	unsigned int numCameras;
+	error = busMgr.GetNumOfCameras(&numCameras);
+	
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	printf("Number of cameras detected: %u\n", numCameras);
+
+	if (numCameras < 1)
+	{
+		printf("Insufficient number of cameras... exiting\n");
+		return -1;
+	}
+
+	PGRGuid guid;
+	error = busMgr.GetCameraFromIndex(0, &guid);
+
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	// Connect to a camera
+	error = cam.Connect(&guid);
+	
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	// Get the camera information
+	CameraInfo camInfo;
+	error = cam.GetCameraInfo(&camInfo);
+	
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	PrintCameraInfo(&camInfo);
+
+	// Query for available Format 7 modes
+	Format7Info fmt7Info;
+	bool supported;
+	fmt7Info.mode = k_fmt7Mode;
+	error = cam.GetFormat7Info(&fmt7Info, &supported);
+	
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	PrintFormat7Capabilities(fmt7Info);
+
+	if ((k_fmt7PixFmt & fmt7Info.pixelFormatBitField) == 0)
+	{
+		// Pixel format not supported!
+		printf("Pixel format is not supported\n");
+		return -1;
+	}
+
+	Format7ImageSettings fmt7ImageSettings;
+	fmt7ImageSettings.mode = k_fmt7Mode;
+	fmt7ImageSettings.offsetX = 0;
+	fmt7ImageSettings.offsetY = 0;
+	fmt7ImageSettings.width = fmt7Info.maxWidth;
+	fmt7ImageSettings.height = fmt7Info.maxHeight;
+	fmt7ImageSettings.pixelFormat = k_fmt7PixFmt;
+
+	bool valid;
+	Format7PacketInfo fmt7PacketInfo;
+
+	// Validate the settings to make sure that they are valid
+	error = cam.ValidateFormat7Settings(
+		&fmt7ImageSettings,
+		&valid,
+		&fmt7PacketInfo);
+		
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	if (!valid)
+	{
+		// Settings are not valid
+		printf("Format7 settings are not valid\n");
+		return -1;
+	}
+
+	// Set the settings to the camera
+	error = cam.SetFormat7Configuration(
+		&fmt7ImageSettings,
+		fmt7PacketInfo.recommendedBytesPerPacket);
+	
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	//settings for version 1.0 fmf header
+	fmfVersion = 1;
+	SizeY = fmt7ImageSettings.height;
+	SizeX = fmt7ImageSettings.width;
+	bytesPerChunk = SizeY*SizeX + sizeof(double);
+	nframes = 0;
+
+	sprintf_s(fname, "E:\\Wing-%d%02d%02dT%02d%02d%02d.fmf", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+	remove(fname);
+		
+	FMF_Out = fopen(fname, "wb");
+		
+	if(FMF_Out == NULL)
+	{
+		printf("\nError opening FMF writer. Recording terminated.");
+		return -1;
+	}
+		
+	//write FMF header data
+	fwrite(&fmfVersion, sizeof(unsigned __int32), 1, FMF_Out);
+	fwrite(&SizeY, sizeof(unsigned __int32), 1, FMF_Out);
+	fwrite(&SizeX, sizeof(unsigned __int32), 1, FMF_Out);
+	fwrite(&bytesPerChunk, sizeof(unsigned __int64), 1, FMF_Out);
+	fwrite(&nframes, sizeof(unsigned __int64), 1, FMF_Out);
+
+	sprintf_s(flogname, "E:\\log-%d%02d%02dT%02d%02d%02d.txt", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+	remove(flogname);
+		
+	flog = fopen(flogname, "w");
+		
+	if(flog == NULL)
+	{
+		printf("\nError creating log file. Recording terminated.");
+		return -1;
+	}
+
+	// Start capturing images
+	error = cam.StartCapture();
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	// Retrieve frame rate property
+	Property frmRate;
+	frmRate.type = FRAME_RATE;
+	error = cam.GetProperty(&frmRate);
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	printf("Frame rate is %3.2f fps\n", frmRate.absValue);
+
+	for (int imageCount = 0; ; imageCount++)
+	{
+		Image rawImage, convertedImage;
+			
+		// Retrieve an image
+		error = cam.RetrieveBuffer(&rawImage);
+		if (error != PGRERROR_OK)
+		{
+			PrintError(error);
+			continue;
+		}
+
+		//get image timestamp
+		timestamp = rawImage.GetTimeStamp();
+		stamp = (double) timestamp.seconds;
+
+		// Convert the raw image
+		error = rawImage.Convert(PIXEL_FORMAT_MONO8, &convertedImage);
+		
+		if (error != PGRERROR_OK)
+		{
+			PrintError(error);
+			return -1;
+		}
+
+		if (record)
+		{
+			if (nframes == 0)
+				printf("\nRecording ...\n");
+		
+			fwrite(&stamp, sizeof(double), 1, FMF_Out);
+			fwrite(convertedImage.GetData(), convertedImage.GetDataSize(), 1, FMF_Out);
+
+			//fprintf(flog, "Frame %d - TimeStamp [%d %d]\n", nframes, timestamp.seconds, timestamp.microSeconds);
+
+			nframes++;
+		}
+
+		if (!record)
+		{
+			// convert to OpenCV Mat
+			unsigned int rowBytes = (double)convertedImage.GetReceivedDataSize() / (double)convertedImage.GetRows();
+			Mat tFrame = Mat(convertedImage.GetRows(), convertedImage.GetCols(), CV_8UC1, convertedImage.GetData(), rowBytes);		
+			frame = tFrame.clone();
+			
+			int width=frame.size().width;
+			int height=frame.size().height;
+
+			line(frame, Point((width/2)-50,height/2), Point((width/2)+50, height/2), 255);  //crosshair horizontal
+			line(frame, Point(width/2,(height/2)-50), Point(width/2,(height/2)+50), 255);  //crosshair vertical
+
+			imshow("raw image", frame);
+			waitKey(1);
+		}
+		
+		if (GetAsyncKeyState(VK_RETURN))			//press [ENTER] to start recording
+			record = true;
+		
+		if (GetAsyncKeyState(VK_ESCAPE))			//press [ESC] to exit
+				break;
+	}
+
+	// Stop capturing images
+	error = cam.StopCapture();
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	// Disconnect the camera
+	error = cam.Disconnect();
+	if (error != PGRERROR_OK)
+	{
+		PrintError(error);
+		return -1;
+	}
+
+	if (record)
+	{
+		printf( "\nFinished grabbing %d images\n", nframes );
+
+		//seek to location in file where nframes is stored and replace
+		fseek (FMF_Out, 20, SEEK_SET );	
+		fwrite(&nframes, sizeof(unsigned __int64), 1, FMF_Out);
+	}
+
+	// close fmf file
+	fclose(FMF_Out);
+
+	//close log file
+	fclose(flog);
+	
+	if (!record)
+	{
+		remove(fname);
+		remove(flogname);
+	}
+	
+	printf("\nDone! Press Enter to exit...\n");
+	getchar();
+
+	return 0;
+}
+
---
+++ wingtrack/wingtrack/wingtrack.cpp
@@ -0,0 +1,127 @@
+// wingtrack.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/background_segm.hpp>
+#include <opencv2/gpu/gpu.hpp>
+
+using namespace cv;
+using namespace cv::gpu;
+
+#define USEGPU 1
+
+FILE *FMF_In;
+unsigned __int32 fmfVersion, SizeY, SizeX;
+unsigned __int64 bytesPerChunk, nframes;
+long maxFramesInFile;
+char *buf;
+
+BackgroundSubtractorMOG mog_cpu;
+MOG_GPU mog_gpu;
+
+Mat frame, fgmask;
+GpuMat d_frame, d_fgmask; 
+
+void ReadFMFHeader()
+{		
+	fread(&fmfVersion, sizeof(unsigned __int32), 1, FMF_In);
+	fread(&SizeY, sizeof(unsigned __int32), 1, FMF_In);
+	fread(&SizeX, sizeof(unsigned __int32), 1, FMF_In);
+	fread(&bytesPerChunk, sizeof(unsigned __int64), 1, FMF_In);
+	fread(&nframes, sizeof(unsigned __int64), 1, FMF_In);
+
+	buf = new char[bytesPerChunk];
+			
+	maxFramesInFile = (unsigned long int)nframes;
+
+	printf(
+		"\n*** VIDEO INFORMATION ***\n"
+		"FMF Version: %d\n"
+		"Height: %d\n"
+		"Width: %d\n"
+		"Frame Size: %d\n"
+		"Number of Frames: %d\n",
+		fmfVersion, 
+		SizeY, 
+		SizeX, 
+		bytesPerChunk-sizeof(double), 
+		nframes);
+}
+
+int ReadFMFFrame(unsigned long frameIndex)
+{
+	if((long)frameIndex>=0L && (long)frameIndex < maxFramesInFile)
+		fseek (FMF_In, frameIndex*bytesPerChunk + 28 , SEEK_SET );
+	else
+		return -1; // Cannot grab .. illegal frame number
+
+	fread(buf, sizeof(double), 1, FMF_In);
+	fread(buf, bytesPerChunk-sizeof(double), 1, FMF_In);
+	
+	return 1;	
+}
+
+void CloseFMF()
+{
+	fclose(FMF_In);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if (argc != 2)
+	{
+		printf("Filename not specified... exiting\n");
+		return -1;
+	}
+
+	FMF_In = fopen(argv[1], "rb");
+	
+	if(FMF_In == NULL) // Cannot open File
+	{
+		printf("Cannot open input video file\n");
+		return -1;	
+	}
+		
+	ReadFMFHeader();
+	
+	for (int imageCount = 0; ; imageCount++)
+	{
+		int success = ReadFMFFrame(imageCount);
+			
+		if (success)
+		{
+			Mat tFrame = Mat(SizeY, SizeX, CV_8UC1, buf, (bytesPerChunk-sizeof(double))/SizeY); //byte size of each row of frame
+			frame = tFrame.clone();
+		}
+		else
+			break;			
+		
+		imshow("raw image", frame);
+
+		if (USEGPU)
+		{
+			d_frame.upload(frame);
+			mog_gpu(d_frame, d_fgmask, 0.01f);
+			d_fgmask.download(fgmask);
+		}
+		else
+			mog_cpu(frame, fgmask, 0.01);
+		
+		imshow("FG mask", fgmask);
+		
+		char key = waitKey(1);
+		
+		if (key == 27)		//press [ESC] to exit
+			break;
+	}
+
+	CloseFMF();
+	
+	printf("\nDone! Press Enter to exit...\n");
+	getchar();
+
+	return 0;
+}
+
