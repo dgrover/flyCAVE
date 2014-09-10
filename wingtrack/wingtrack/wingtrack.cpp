13f03a40618c90eb2c426e7e180c395a0fd9df1c
Dhruv Grover <dhgrover@ucsd.edu>
2014-09-09 21:28:06 -0700

    working version of wing beat motion blur

--- wingtrack/wingtrack/wingtrack.cpp
+++ wingtrack/wingtrack/wingtrack.cpp
@@ -56,7 +56,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 
-	Mat frame, fg, bg, body_mask, outer_mask;
+	Mat frame, dframe, fg, bg, body_mask, fly_blob, outer_mask;
 
 	printf("\nComputing background model... ");
 	bg = Mat::zeros(Size(imageWidth, imageHeight), CV_32FC1);
@@ -76,10 +76,30 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	printf("Done\n");
 
+	int idx;
+
 	threshold(bg, body_mask, 100, 255, CV_THRESH_BINARY);
+	threshold(bg, fly_blob, 100, 255, CV_THRESH_BINARY_INV);
+
+	std::vector<std::vector<cv::Point> > contours;
+	cv::findContours(fly_blob, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	/// Find the rotated rectangles and ellipses for each contour
+	vector<RotatedRect> minEllipse(contours.size());
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		if (contours[i].size() > 50)
+		{
+			minEllipse[i] = fitEllipse(Mat(contours[i]));
+			ellipse(fly_blob, minEllipse[i], Scalar::all(255), 1, 8);
+
+			idx = i;
+		}
+	}
 
 	outer_mask = Mat::zeros(Size(imageWidth, imageHeight), CV_8UC1);
-	circle(outer_mask, Point(imageWidth / 2, imageHeight / 2), imageWidth / 2, Scalar(255, 255, 255), CV_FILLED);
+	circle(outer_mask, minEllipse[idx].center, imageWidth / 2, Scalar(255, 255, 255), CV_FILLED);
 
 
 	Mat erodeElement = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
@@ -92,6 +112,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		else
 			frame = wingcam.GrabFrame();
 
+		dframe = frame.clone();
+
 		//apply body mask to frame and bg
 		frame &= body_mask;
 		
@@ -100,12 +122,18 @@ int _tmain(int argc, _TCHAR* argv[])
 		
 		fg &= outer_mask;
 
-		erode(fg, fg, erodeElement, Point(-1, -1), 2);
-		dilate(fg, fg, dilateElement, Point(-1, -1), 2);
+		erode(fg, fg, erodeElement, Point(-1, -1), 3);
+		dilate(fg, fg, dilateElement, Point(-1, -1), 3);
 
 		// Find contours
 		std::vector<std::vector<cv::Point> > contours;
 		cv::findContours(fg, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+		Point tl = minEllipse[idx].center;
+		Point tr = minEllipse[idx].center;
+		Point bl = minEllipse[idx].center;
+		Point br = minEllipse[idx].center;
+		
 
 		// Find the convex hull object for each contour
 		vector<vector<Point> >hull(contours.size());
@@ -113,14 +141,52 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 			if (contours[i].size() > 50)
 			{
-				drawContours(fg, contours, i, Scalar::all(255), 1, 8, vector<Vec4i>(), 0, Point());
+				//drawContours(fg, contours, i, Scalar::all(255), 1, 8, vector<Vec4i>(), 0, Point());
 				convexHull(Mat(contours[i]), hull[i], false);
-				drawContours(frame, hull, i, Scalar::all(255), 1, 8, vector<Vec4i>(), 0, Point());
+				//drawContours(frame, hull, i, Scalar::all(255), 1, 8, vector<Vec4i>(), 0, Point());
+
+				for (int j = 0; j < hull[i].size(); j++)
+				{
+					double res1 = cv::norm(tl);
+					double res2 = cv::norm(hull[i][j]);
+
+					if (res2 < res1)
+						tl = hull[i][j];
+				}
+
+				for (int j = 0; j < hull[i].size(); j++)
+				{
+					double res1 = cv::norm(Point (imageWidth, 0) - tr);
+					double res2 = cv::norm(Point(imageWidth, 0) - hull[i][j]);
+
+					if (res2 < res1)
+						tr = hull[i][j];
+				}
+
+				for (int j = 0; j < hull[i].size(); j++)
+				{
+					double res = cv::norm(Point(minEllipse[idx].center.x, imageHeight) - hull[i][j]);
+					double res1 = cv::norm(Point(minEllipse[idx].center.x, imageHeight) - bl);
+					double res2 = cv::norm(Point(minEllipse[idx].center.x, imageHeight) - br);
+
+					if (res < res1 && hull[i][j].x < minEllipse[idx].center.x)
+						bl = hull[i][j];
+
+					if (res < res2 && hull[i][j].x > minEllipse[idx].center.x)
+						br = hull[i][j];
+				}
+
 			}
 		}
-		
-		imshow("raw image", frame);
-		imshow("foreground mask", fg);
+
+		line(dframe, minEllipse[idx].center, tl, Scalar::all(0), 2);
+		line(dframe, minEllipse[idx].center, bl, Scalar::all(0), 2);
+		line(dframe, minEllipse[idx].center, tr, Scalar::all(0), 2);
+		line(dframe, minEllipse[idx].center, br, Scalar::all(0), 2);
+
+
+		imshow("raw image", dframe);
+		//imshow("foreground mask", fg);
 
 		waitKey(1);
 
@@ -138,4 +204,3 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	return 0;
 }
-
